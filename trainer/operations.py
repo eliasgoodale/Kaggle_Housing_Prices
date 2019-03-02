b151310a98cd0f556772a2427ea0d268ca479097
snugitup <eligoodale@yahoo.com>
2019-03-01 20:11:08 -0800

    finished writing compose functions

--- trainer/operations.py
+++ trainer/operations.py
@@ -1,27 +1,35 @@
 import numpy as np
-
+from functools import reduce
 processing_dict = {
     "first": [
         {
-            "op": "prod",
+            "op_name": "multiply",
             "targets": [10, 2],
         },
         {
-            "op": "sum",
-            "targets": ["result", 12]
+            "op_name": "add",
+            "targets": [12]
         }
     ]
 }
 
-for key, value in processing_dict.items():
-    pipeline = np.array(value)
-    result = 0
-    for operation in pipeline:
-        if "result" in operation["targets"]:
-            idx = operation["targets"].index("result")
-            operation["targets"][idx] = result
 
-        func = getattr(np, operation["op"])
-        result = func(operation["targets"]).astype(np.int)
-    print(result)
+
+def compose(*functions):
+    return reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
+
+for key, value in processing_dict.items():
+    pipeline = value
+    for idx, op in enumerate(pipeline):
+        func = getattr(np, op["op_name"])
+        if idx == 0:
+            result = reduce(func, op["targets"])
+        else:
+            result = reduce(func, [result, *op["targets"]])
+
+print (result)
+    
+    #pipeline = value
+    #result = reduce(evaluate, pipeline)
+    #print(result)
 
